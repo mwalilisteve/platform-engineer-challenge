e060b64efc6adbfa7b49e6a93a5386b09791dbf0
Steve Mwalili <stevemwalili@NDK561-Steve-mac.local>
2026-05-10 11:14:04 +0300

    replace module variable

--- terraform/modules/eks-cluster/variables.tf
+++ terraform/modules/eks-cluster/variables.tf
@@ -14,24 +14,59 @@ variable "environment" {
 }
 
 variable "vpc_id" {
-  description = "ID of the VPC"
+  description = "VPC ID to deploy the cluster into"
   type        = string
 }
 
 variable "subnet_ids" {
-  description = "Subnet IDs for the EKS control plane"
+  description = "Private subnet IDs for the EKS control plane"
   type        = list(string)
 }
 
 variable "node_group_subnet_ids" {
-  description = "Subnet IDs for worker node groups (should be private)"
+  description = "Subnet IDs for the managed node group"
   type        = list(string)
 }
 
-variable "app_bucket_name" {
-  description = "S3 bucket name for IRSA policy"
+variable "node_instance_type" {
+  description = "EC2 instance type for the managed node group"
   type        = string
-  default     = ""
+  default     = "t3.medium"
+}
+
+variable "node_desired_size" {
+  description = "Desired number of nodes in the managed node group"
+  type        = number
+  default     = 1
+}
+
+variable "node_min_size" {
+  description = "Minimum number of nodes in the managed node group"
+  type        = number
+  default     = 1
+}
+
+variable "node_max_size" {
+  description = "Maximum number of nodes in the managed node group"
+  type        = number
+  default     = 3
+}
+
+variable "app_sa_namespace" {
+  description = "Kubernetes namespace the app service account lives in"
+  type        = string
+  default     = "default"
+}
+
+variable "app_sa_name" {
+  description = "Kubernetes service account name that will assume the IRSA role"
+  type        = string
+  default     = "app-sa"
+}
+
+variable "app_bucket_name" {
+  description = "Name of the S3 bucket the app-sa service account needs access to"
+  type        = string
 }
 
 variable "tags" {
