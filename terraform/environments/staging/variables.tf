00a3693d0254c8dc7d2bf5fd6e62da3e3fc150ae
Steve Mwalili <stevemwalili@NDK561-Steve-mac.local>
2026-05-10 11:16:53 +0300

    add IRSA output & backend

--- terraform/environments/staging/variables.tf
+++ terraform/environments/staging/variables.tf
@@ -40,7 +40,55 @@ variable "cluster_version" {
   default     = "1.30"
 }
 
+variable "node_instance_type" {
+  description = "EC2 instance type for the managed node group"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "node_desired_size" {
+  description = "Desired number of nodes in the managed node group"
+  type        = number
+  default     = 1
+}
+
+variable "node_min_size" {
+  description = "Minimum number of nodes in the managed node group"
+  type        = number
+  default     = 1
+}
+
+variable "node_max_size" {
+  description = "Maximum number of nodes in the managed node group"
+  type        = number
+  default     = 3
+}
+
+variable "app_sa_namespace" {
+  description = "Kubernetes namespace the app service account lives in"
+  type        = string
+  default     = "default"
+}
+
+variable "app_sa_name" {
+  description = "Kubernetes service account name that will assume the IRSA role"
+  type        = string
+  default     = "app-sa"
+}
+
 variable "app_bucket_name" {
   description = "Name of the S3 bucket the application service account needs access to"
   type        = string
+}
+
+variable "state_bucket" {
+  description = "S3 bucket name used for Terraform remote state"
+  type        = string
+  default     = "acme-terraform-state"
+}
+
+variable "state_lock_table" {
+  description = "DynamoDB table name used for Terraform state locking"
+  type        = string
+  default     = "acme-terraform-locks"
 }
