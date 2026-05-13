98cc45c99a97444887af386f4b58288385873006
Steve Mwalili <stevemwalili@NDK561-Steve-mac.local>
2026-05-13 07:51:27 +0300

    update

--- terraform/modules/eks-cluster/main.tf
+++ terraform/modules/eks-cluster/main.tf
@@ -36,7 +36,7 @@ resource "aws_eks_cluster" "this" {
   vpc_config {
     subnet_ids              = var.subnet_ids
     endpoint_private_access = true
-    endpoint_public_access  = true
+    endpoint_public_access  = false
   }
 
   depends_on = [aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy]
